e3a023c8260d3a13140f2d7cd6a80d8311fd4d72
Fares Qaddoura <frqd92@gmail.com>
2022-12-29 19:46:23 +0000

    complete structure and layout, adds complete functionality to select menus, functions to detect which mark and type each player is

--- script.js
+++ script.js
@@ -13,52 +13,53 @@ const GameBoard = (function(){
         }
 
     function turnRouter(e){
-        let player1 = checkPlayers()[0];
-        let player2 = checkPlayers()[1];
-
-
+        let player1 = checkPlayerMark()[0];
+        let player2 = checkPlayerMark()[1];
+        let player1Type =checkPlayerType()[0];
+        let player2Type =checkPlayerType()[1];
     }
 
-    function checkPlayers(){
+    function checkPlayerMark(){
         const toggle = document.querySelector(".toggle");
-        if(!toggle.classList.contains("toggle-O")){
+        if(!toggle.classList.contains("toggle-1-O")){
             return ["X","O"];
         }
         else{
             return ["O","X"];
         }
     }
-
-
+    function checkPlayerType(){
+        let left, right;
+        const leftPlayer = document.querySelector(".drop-text-1").textContent;
+        const rightPlayer = document.querySelector(".drop-text-2").textContent;
+        return [leftPlayer, rightPlayer]
+    }
     document.getElementById("history-btn").addEventListener("click", ()=>{
-     
-    console.log(checkPlayers()[0])
+    console.log(checkPlayerMark()[0], checkPlayerType()[0])
+    console.log(checkPlayerMark()[1], checkPlayerType()[1])
     })
-
     return {createBoard}
 })
-
 
 
 
 const game = GameBoard();
 game.createBoard()
 //------------------------------------------------------------------------------------------//
+//Reset Button
 const resetBtn = document.getElementById("reset-btn");
 resetBtn.addEventListener("click", ()=>{game.createBoard()});
 //------------------------------------------------------------------------------------------//
+//toggle player
 const toggleContainer = document.querySelectorAll(".toggle-container");
-
-toggleContainer.forEach(elem=>{
-    elem.addEventListener("click", toggleLogic, false);
-})
+toggleContainer.forEach(elem=>elem.addEventListener("click", toggleLogic));
 function toggleLogic(e){
     game.createBoard();
     toggle1 = document.querySelector(".toggle-1")
     toggle2 = document.querySelector(".toggle-2");
-    toggle1.classList.toggle("toggle-O");
+    toggle1.classList.toggle("toggle-1-O");
     toggle2.classList.toggle("toggle-2-X");
-    if(toggle1.classList.contains("toggle-O")){
+    if(toggle1.classList.contains("toggle-1-O")){
         toggle1.textContent="O";
         toggle2.textContent="X"
     }
@@ -66,29 +67,29 @@ function toggleLogic(e){
         toggle1.textContent="X";
         toggle2.textContent="O"
     }
-
 }
+//------------------------------------------------------------------------------------------//
 
-function toggleLogicz(e){
-    let tog;
-    let tar = e.target;
-    game.createBoard();
-    if(tar.querySelector(".toggle-2")!=null || tar.classList.contains("toggle-2")){
-        tog=document.querySelector(".toggle-2");
-        tog.classList.toggle("toggle-2-X");
-
-
-
+const selectMenu=function(className, index){
+    const select = document.querySelector(`.${className}`);
+    const menu = document.querySelector(`.menu-${index}`);
+    const li = menu.querySelectorAll("li");
+    const text = document.querySelector(`.drop-text-${index}`)
+    select.addEventListener("click", showMenu, {once:true});
+    function showMenu(){
+        menu.style.visibility="visible";
+        li.forEach(elem=>{
+            elem.textContent===text.textContent?elem.style.background="rgba(0, 21, 255, 0.356)": elem.style.background="";
+            elem.addEventListener("click", liFunc, {once:true})
+        })
     }
-    else{
-        tog=document.querySelector(".toggle-1");
-        tog.classList.toggle("toggle-O");
-        tog.classList.contains("toggle-O")?tog.textContent="O":tog.textContent="X";
+    function liFunc(e){
+        e.stopPropagation();
+        text.textContent = e.target.textContent
+        menu.style.visibility="hidden";
+        select.addEventListener("click", showMenu, {once:true});
     }
+}   
+const selectLeft = selectMenu("drop-1", 1);
+const selectRight = selectMenu("drop-2", 2);
 
-
-}
-
-// toggleContainer.addEventListener("click", ()=>{
-
-// });
