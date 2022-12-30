3461d42238c071f8d9e3dc98d44b2e5ec1147098
Fares Qaddoura <frqd92@gmail.com>
2022-12-30 15:36:35 +0000

    add more logic to the puzzling chaos between two AIs and a human

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const AI = function(board){
 let turn=1;
 let indexBoard = [];
 const GameBoard = (function(){
+
     function createBoard(){ // start/resets board
         document.querySelector(".board-container").innerHTML="";
         turn=1;
@@ -25,57 +26,49 @@ const GameBoard = (function(){
             square.id=i;
             square.classList.add("square");
             document.querySelector(".board-container").appendChild(square);
-            square.addEventListener("click", turnRouter, {once:true});
+            square.addEventListener("click", turnClickRouter, {once:true});
             }
         }
-
-    function turnRouter(e){
-        let squareId = e.target.id;
-        let player1 = checkPlayerMark()[0];
-        let player2 = checkPlayerMark()[1];
-        let player1Type =checkPlayerType()[0];
-        let player2Type =checkPlayerType()[1];
-        let aiPlayer = AI(indexBoard);
-
-        if(turn>9){
-            console.log(turn)
-        }
-     
-        if(player1Type==="Human" && player2Type==="Human"){                            //Human vs Human
-            turn%2===0?playTurn(squareId,player2):playTurn(squareId,player1);
-        }
-
-
-        if(player1Type.includes("AI") || player2Type.includes("AI")){
-            if(player1Type==="Human" || player2Type==="Human"){                    //human vs AI
-
-                if(player1Type==="Human"){
-                    playTurn(squareId,player1)
-                    setTimeout(()=>playTurn(aiPlayer.random(), player2),300);
+  
+    function turnClickRouter(e){
+        clickRouter(e.target.id) 
+    }
+    function menuRouter(){
+        clickRouter(null)
+    }
+    function clickRouter(id){
+        const player1 = playerObjects()[0];
+        const player2 = playerObjects()[1];
+        const ai = AI(indexBoard);
+        if(id!==null){
+            //Human vs Human
+            if(player1.type==="Human" && player2.type==="Human"){       
+                turn%2!==0?playTurn(id, "X"):playTurn(id, "O");
+            }
+            //Human vs AI (if Human is X), only way to change X to ai is from the menu buttons, so menuRouter had to be created. As this function is only executed from square clicking
+            if((player1.type==="Human" || player2.type==="Human") && (player1.type!=="Human" || player2.type!=="Human")){
+                if( (checkPlayerMark().indexOf("X")===0 && checkPlayerType()[1].includes("AI")) || 
+                    (checkPlayerMark().indexOf("X")===1 && checkPlayerType()[0].includes("AI"))){
+                    playTurn(id, "X");
+                    playTurn(ai.random(), "O");
                 }
                 else{
-                    playTurn(squareId,player2)
-                    setTimeout(()=>playTurn(aiPlayer.random(), player1),300);
+                    playTurn(id, "O");
+                    playTurn(ai.random(), "X");
                 }
-
             }
         }
-        
-    }
-    //where you left off
-    function selectRouter(type, select){ //in case player type is changed to ai midgame
-        let ai = AI(indexBoard);
-        console.log(select)
-        console.log(document.querySelector(`.${select}`).innerText)
-        
-        if(turn%2==0 && document.querySelector(`.${select}`).innerText.includes("AI")){
-            playTurn(ai.random(), "O")
-       }
-       else if(turn>1){
-        playTurn(ai.random(), "X")
- 
-       }
-    };
+        else{
+            if( (checkPlayerMark().indexOf("X")===0 && checkPlayerType()[0].includes("AI")) || 
+                (checkPlayerMark().indexOf("X")===1 && checkPlayerType()[1].includes("AI"))){
+                playTurn(ai.random(), "X");
+            }
+        }
+        }
+
+
+
+
 
 
     let testarr = [];
@@ -87,12 +80,7 @@ const GameBoard = (function(){
  
         testarr.push(player)
         testing.innerHTML = `
-        arr: ${indexBoard}--
-        turn: ${turn}--------------
-        plays:
-        ${testarr}
-
-        `
+        arr: ${indexBoard} <br> turn: ${turn} <br> plays: ${testarr} <br>`
     }
 
     function checkPlayerMark(){
@@ -110,6 +98,26 @@ const GameBoard = (function(){
         const rightPlayer = document.querySelector(".drop-text-2").textContent;
         return [leftPlayer, rightPlayer]
     }
+    function playerObjects(){
+        const input1 = document.getElementById("input-1").value
+        const input2 = document.getElementById("input-2").value
+        let player1 = {
+            name: input1,
+            type: checkPlayerType()[0],
+            marker:checkPlayerMark()[0]
+        }
+        let player2 = {
+            name: input2,
+            type: checkPlayerType()[1],
+            marker: checkPlayerMark()[1]
+    
+        }
+        return [player1, player2]
+    }
+
+
+
+
 
     document.getElementById("history-btn").addEventListener("click", (e)=>{
     // console.log(checkPlayerMark()[0], checkPlayerType()[0])
@@ -119,8 +127,13 @@ const GameBoard = (function(){
     console.log("turn: " + turn)
 
     })
-    return {createBoard, turnRouter, selectRouter}
+    return {createBoard, menuRouter}
 })
+
+
+
+
+
 
 
 
@@ -172,10 +185,66 @@ const selectMenu=function(className, index){
         select.addEventListener("click", showMenu, {once:true});
         if(e.target.textContent.includes("AI")){
             const aiChange = GameBoard();
-            aiChange.selectRouter(e.target.textContent, `drop-${index}`);
+            aiChange.menuRouter(e.target.textContent);
         }
 
     }
 }   
 const selectLeft = selectMenu("drop-1", 1);
 const selectRight = selectMenu("drop-2", 2);
+
+
+
+
+/*
+
+        let player1 = checkPlayerMark()[0];
+        let player2 = checkPlayerMark()[1];
+        let player1Type =checkPlayerType()[0];
+        let player2Type =checkPlayerType()[1];
+        let aiPlayer = AI(indexBoard);
+
+        if(turn>9){
+            console.log(turn)
+        }
+     
+        if(player1Type==="Human" && player2Type==="Human"){                            //Human vs Human
+            turn%2===0?playTurn(squareId,player2):playTurn(squareId,player1);
+        }
+
+----------------------------
+
+        if(player1Type.includes("AI") || player2Type.includes("AI")){
+            if(player1Type==="Human" || player2Type==="Human"){                    //human vs AI
+
+                if(player1Type==="Human"){
+                    playTurn(squareId,player1)
+                    setTimeout(()=>playTurn(aiPlayer.random(), player2),300);
+                }
+                else{
+                    playTurn(squareId,player2)
+                    setTimeout(()=>playTurn(aiPlayer.random(), player1),300);
+                }
+
+            }
+        }
+----------------------------------
+    //where you left off
+    function selectRouter(type, select){ //in case player type is changed to ai midgame
+        let ai = AI(indexBoard);
+        console.log(select)
+        console.log(document.querySelector(`.${select}`).innerText)
+        
+        if(turn%2==0 && document.querySelector(`.${select}`).innerText.includes("AI")){
+            playTurn(ai.random(), "O")
+       }
+       else if(turn>1){
+        playTurn(ai.random(), "X")
+ 
+       }
+    };
+
+
+
+
+*/
